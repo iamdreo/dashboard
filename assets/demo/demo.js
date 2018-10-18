082fe90975ab67b46ac33f63d2daccaaf54dc1ab
DRE <andrew_ole@yahoo.com>
2018-10-18 13:55:33 +0100

    First commit

--- assets/demo/demo.js
+++ assets/demo/demo.js
@@ -736,8 +736,8 @@ demo = {
     color = Math.floor((Math.random() * 4) + 1);
 
     $.notify({
-      icon: "tim-icons icon-bell-55",
-      message: "Welcome to <b>Black Dashboard</b> - a beautiful freebie for every web developer."
+      icon: "",
+      message: ""
 
     }, {
       type: type[color],
